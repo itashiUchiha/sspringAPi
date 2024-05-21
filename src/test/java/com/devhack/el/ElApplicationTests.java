e886a66f7e10c1e11bd32134b6e902e7affe9c1c
cheikh <cheikhsidibe13@gmail.com>
2024-05-21 22:40:39 +0000

    setup application

---
+++ src/test/java/com/devhack/el/ElApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devhack.el;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
